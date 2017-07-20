ef4dad8059a7bea8b95b5f032a38a9ecab22f6c8
wuzhong <wuzhong@wanjiedata.com>
2017-07-20 12:05:59 +0800

    callback&&library

---
+++ JavaScript/ES5/JS-deep/library/library.js
@@ -0,0 +1,7 @@
+/**
+ * javascript library
+ * createDate: 2017/07/20
+ * author: NARUTOne
+ *
+ */
+
---
+++ JavaScript/ES6/ES6.md
@@ -0,0 +1,3 @@
+### Welcome to ES6
+
+-[JavaScript Promise迷你书](http://liubin.org/promises-book/)
