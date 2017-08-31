608960cac4b80400070459eedc9b95cf0d3d1347
NARUTOne <wznaruto326@163.com>
2017-08-31 16:19:16 +0800

    update some :ant:

--- framewords/ReactJS/mytest/functions/Overview/Todos/index.js
+++ framewords/ReactJS/mytest/functions/Overview/Todos/index.js
@@ -11,12 +11,21 @@ class Todos extends Component {
     super()
     this.update = update.bind(this)
     this.state = {
-      list: []
+      list: [],
+      count:0
     }
   }
 
   handleListChange(type, value) {
     this.update(type, 'list', value)
+  }
+
+  handelStateClick() {
+    // 第二个参数, 回调函数获取最新state
+    let count = this.state.count + 1
+    this.setState({count: count}, () => {
+      console.log(this.state.count)
+    }) 
   }
 
   render() {
@@ -27,6 +36,8 @@ class Todos extends Component {
         <Input onChange={e => update('set', 'text', e.target.value)} />
         <Button onClick={() => update('push', 'list', text)}>添加</Button>
         <List data={list} onChange={::this.handleListChange} />
+
+        <Button onClick={::this.handelStateClick}>测试state</Button>
       </div>
     )
   }
