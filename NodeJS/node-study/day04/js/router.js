b721e32dc8a18519ff4c56a03deb47e1c84c4280
NARUTOne <wznaruto326@163.com>
2017-10-13 19:23:22 +0800

    rename

--- NodeJS/node-study/day04/js/router.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by BFD_707 on 2016/2/3.
- */
-function route(pathname){
-    console.log('About to route a request for'+pathname);
-}
-exports.route = route;
--- NodeJS/node.md
+++
@@ -1,3 +0,0 @@
-## nodeJS about 
-
-- [Yarn 介绍](http://qianduan.guru/2016/11/09/yarn-vs-npm/)
